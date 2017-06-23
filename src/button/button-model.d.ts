4f577eaccc6308b9aefe85e8e24ce02b6e6b486c
pipeline <pipeline@syncfusion.com>
2017-06-23 10:27:04 +0000

    bug(ej2-2573): outline issue in firefox fixed
    committed firefox outline issue with jenkin changes

--- src/button/button-model.d.ts
+++ src/button/button-model.d.ts
@@ -1,4 +1,4 @@
-import { Property, NotifyPropertyChanges, INotifyPropertyChanged, Component } from '@syncfusion/ej2-base';import { CreateBuilder, ripple, EventHandler } from '@syncfusion/ej2-base';import { createElement, addClass, removeClass, attributes, detach } from '@syncfusion/ej2-base/dom';import { ButtonHelper } from './button-builder';
+import { Property, NotifyPropertyChanges, INotifyPropertyChanged, Component } from '@syncfusion/ej2-base';import { CreateBuilder, ripple, EventHandler } from '@syncfusion/ej2-base';import { createElement, addClass, removeClass, attributes, detach } from '@syncfusion/ej2-base/dom';import { ButtonHelper } from './button-builder';
 import {IconPosition} from "./button";
 import {ComponentModel} from '@syncfusion/ej2-base';
 
@@ -8,24 +8,24 @@ import {ComponentModel} from '@syncfusion/ej2-base';
 export interface ButtonModel extends ComponentModel{
 
     /**
-     * Positions icon before / after text content in button.     * When set to `right` the icon will be positioned right to text content.     * @default "IconPosition.left"     */    iconPosition?: IconPosition;
+     * Positions icon before / after text content in button.     * When set to `right` the icon will be positioned right to text content.     * @default "IconPosition.left"     */    iconPosition?: IconPosition;
 
     /**
-     * Defines class / multiple classes separated by space for button which is used to include icon in button.     * Button can include sprite image, font icon.     * @default ""     */    iconCss?: string;
+     * Defines class / multiple classes separated by space for button which is used to include icon in button.     * Button can include sprite image, font icon.     * @default ""     */    iconCss?: string;
 
     /**
-     * Indicates button is in `disabled` state. When set to `true` button will be in `disabled` state.     * @default false     */    disabled?: boolean;
+     * Indicates button is in `disabled` state. When set to `true` button will be in `disabled` state.     * @default false     */    disabled?: boolean;
 
     /**
-     * Makes button visually prioritized. When set to `true`, button will look as more prioritized visually.     * @default false     */    isPrimary?: boolean;
+     * Makes button visually prioritized. When set to `true`, button will look as more prioritized visually.     * @default false     */    isPrimary?: boolean;
 
     /**
-     * Adds class / multiple classes separated by space in button element. Types and styles of button can be defined with this.     * @default ""     */    cssClass?: string;
+     * Adds class / multiple classes separated by space in button element. Types and styles of button can be defined with this.     * @default ""     */    cssClass?: string;
 
     /**
-     * Defines the inner content of the button element.     * @default ""     */    content?: string;
+     * Defines the inner content of the button element.     * @default ""     */    content?: string;
 
     /**
-     * Makes the button as toggle button, When you set to `true`. When you click it, the state changes from normal to active state.     * @default false     */    isToggle?: boolean;
+     * Makes the button as toggle button, When you set to `true`. When you click it, the state changes from normal to active state.     * @default false     */    isToggle?: boolean;
 
 }
