06349f296e1ca904ac09159e1a0f505d75bfeff9
pipeline <pipeline@syncfusion.com>
2017-08-29 11:02:25 +0000

    feature(EJ2-3646): fabric theme

--- src/button/button-model.d.ts
+++ src/button/button-model.d.ts
@@ -8,24 +8,47 @@ import {ComponentModel} from '@syncfusion/ej2-base';
 export interface ButtonModel extends ComponentModel{
 
     /**
-     * Positions the icon before/after the text content in the Button.     * When set to `right`, the icon will be positioned to the right of the text content.     * @default "left"     */    iconPosition?: IconPosition;
+     * Positions the icon before/after the text content in the Button.
+     * When set to `right`, the icon will be positioned to the right of the text content.
+     * @default "left"
+     */
+    iconPosition?: IconPosition;
 
     /**
-     * Defines class/multiple classes separated by a space for the Button that is used to include an icon.     * Buttons can also include font icon and sprite image.     * @default ""     */    iconCss?: string;
+     * Defines class/multiple classes separated by a space for the Button that is used to include an icon.
+     * Buttons can also include font icon and sprite image.
+     * @default ""
+     */
+    iconCss?: string;
 
     /**
-     * Specifies a value that indicates whether the Button control is `disabled` or not.     * @default false     */    disabled?: boolean;
+     * Specifies a value that indicates whether the Button control is `disabled` or not.
+     * @default false
+     */
+    disabled?: boolean;
 
     /**
-     * Allows the appearance of the Button to be enhanced and visually appealing when set to `true`.     * @default false     */    isPrimary?: boolean;
+     * Allows the appearance of the Button to be enhanced and visually appealing when set to `true`.
+     * @default false
+     */
+    isPrimary?: boolean;
 
     /**
-     * Defines class/multiple classes separated by a space in the Button element. Button types, styles, and size can be defined using this.     * @default ""     */    cssClass?: string;
+     * Defines class/multiple classes separated by a space in the Button element. Button types, styles, and size can be defined using this.
+     * @default ""
+     */
+    cssClass?: string;
 
     /**
-     * Defines the `content` of the Button element that can either be text or HTML elements.     * @default ""     */    content?: string;
+     * Defines the `content` of the Button element that can either be text or HTML elements.
+     * @default ""
+     */
+    content?: string;
 
     /**
-     * Makes the Button toggle, when set to `true`. When you click it, the state changes from normal to active.     * @default false     */    isToggle?: boolean;
+     * Makes the Button toggle, when set to `true`. When you click it, the state changes from normal to active.
+     * @default false
+     */
+    isToggle?: boolean;
 
 }
